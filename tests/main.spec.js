9cff893cabd591e18400d56a219a48978263c946
myok12 <myok12@yahoo.com>
2013-04-01 16:10:42 -0400

    supporting tests, although not working

---
+++ tests/main.spec.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    var assert = require("assert");
+    var main = require("../frontend/js/main");
+
+    describe("main", function () {
+        it("exports fn", function () {
+            console.log(main);
+        });
+    });
+}());
